c4eb83c1d829d618d62c7c196660544bdbd7ad98
Vahagn Martirosyan <vahagn@Vahagns-MacBook-Pro.local>
2023-02-24 14:09:03 +0400

    Add provider.tf file

---
+++ examples/prefect/provider.tf
@@ -0,0 +1,14 @@
+# This is example configuration of provider
+terraform {
+  required_providers {
+    prefect = {
+      version = "0.2"
+      source  = "miandevops/edu/prefect"
+    }
+  }
+}
+
+provider "prefect" {
+  url = "http[s]://<url>"
+  #url = "http://localhost:4200"
+}
