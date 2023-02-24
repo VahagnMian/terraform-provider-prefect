0daee88cd69b31aeec1c513d2a7b0f3d6e869a36
Vahagn Martirosyan <vahagn@Vahagns-MacBook-Pro.local>
2023-02-24 13:24:17 +0400

    Add ability to specify url from provider configuration

--- examples/prefect/main.tf
+++ examples/prefect/main.tf
@@ -2,6 +2,18 @@ data "prefect_work_queues" "work_queues" {
 
 }
 
+resource "prefect_work_queue" "wq" {
+    name        = "workqueue2"
+    description = "Description2"
+    concurrency_limit = 50
+}
+
+resource "prefect_work_queue" "wq1" {
+    name        = "workqueue3"
+    description = "Description3"
+    concurrency_limit = 50
+}
+
 output "work_queues" {
-  value = data.prefect_work_queues.work_queues
+    value = data.prefect_work_queues.work_queues
 }
